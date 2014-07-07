972ef5d04f7a904a28cc720fdf771297bbebfc2d
dualmoon <dual@gmx.com>
2014-07-07 06:15:01 +0000

    Update layout to use theme. Update package.json version. Update package.json engines. Removed debug json from page.

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. 
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
-});*/
-
 router.get('/', function(req, res) {
    res.render('generate');
 });
